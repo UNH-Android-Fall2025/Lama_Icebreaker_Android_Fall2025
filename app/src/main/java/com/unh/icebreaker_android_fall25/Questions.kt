4623dcb526166d6492d4d66c6debe4d409d21dbe
CopyCodeWithMe <razzzzzlama@gmail.com>
2025-09-21 23:46:00 -0400

    Created question model

---
+++ app/src/main/java/com/unh/icebreaker_android_fall25/Questions.kt
@@ -0,0 +1,6 @@
+package com.unh.icebreaker_android_fall25
+
+data class Questions(
+    val text: String = "", // Question content from database
+    val id: String = "" // Unique hash identifier within database
+)
