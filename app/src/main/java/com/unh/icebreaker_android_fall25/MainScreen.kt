6247c9e5e25c617dcb13d5f3385d8ab0ec1fd7fb
CopyCodeWithMe <razzzzzlama@gmail.com>
2025-09-22 00:14:56 -0400

    Improved visualization
    View, Model, and Controller are created and linked. Need to create database and read and write to it.

--- app/src/main/java/com/unh/icebreaker_android_fall25/MainScreen.kt
+++ app/src/main/java/com/unh/icebreaker_android_fall25/MainScreen.kt
@@ -17,6 +17,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -34,6 +35,7 @@ fun MainScreen(
     var prefName by remember { mutableStateOf("") }
     var question by remember { mutableStateOf("") }
     var answer by remember { mutableStateOf("") }
+    val focusManager = LocalFocusManager.current
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -109,6 +111,11 @@ fun MainScreen(
         Button(
             onClick = {
                 onSubmitClicked()
+                firstName = ""
+                lastName = ""
+                prefName = ""
+                answer = ""
+                focusManager.clearFocus()
             },
             modifier = Modifier.padding(top = 20.dp),
             colors = ButtonDefaults.buttonColors(
