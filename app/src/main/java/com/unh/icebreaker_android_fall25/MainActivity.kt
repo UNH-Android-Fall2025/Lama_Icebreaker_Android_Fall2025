d13622d8b1ce192f5fcadfd21330b0c5bac518f3
CopyCodeWithMe <razzzzzlama@gmail.com>
2025-09-22 14:00:14 -0400

    Can fetch from DB

--- app/src/main/java/com/unh/icebreaker_android_fall25/MainActivity.kt
+++ app/src/main/java/com/unh/icebreaker_android_fall25/MainActivity.kt
@@ -12,9 +12,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
 import com.unh.icebreaker_android_fall25.ui.theme.IcebreakerandroidFall25Theme
 
 class MainActivity : ComponentActivity() {
+    private val db = Firebase.firestore
+    private var questionBank: MutableList<Questions>? = arrayListOf()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -32,6 +37,19 @@ class MainActivity : ComponentActivity() {
     }
     private fun getQuestionsFromFirebase(){
         Log.d("IcebreakerF2025","Get from DB")
+        db.collection("Questions")
+            .get()
+            .addOnSuccessListener { result ->
+                questionBank = mutableListOf()
+                for(document in result) {
+                    val question = document.toObject(Questions::class.java)
+                    questionBank!!.add(question)
+                    Log.d("IcebreakerF2025","$question")
+                }
+            }
+            .addOnFailureListener { error ->
+                Log.d("IcebreakerF2025", "error", error)
+            }
     }
 
     private fun setResponseToFirebase(){
