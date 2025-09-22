f9c2ae4ccb31150b3f07e62b9e6854c77b0e4eb1
CopyCodeWithMe <razzzzzlama@gmail.com>
2025-09-21 20:39:19 -0400

    Initial Commit
    This project is going to be Icebreaker built in Android during the fall semester of 2025. It will use Kotlin Compose for the user interface and feature Firebase for the database.

---
+++ app/src/main/java/com/unh/icebreaker_android_fall25/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.unh.icebreaker_android_fall25
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.unh.icebreaker_android_fall25.ui.theme.IcebreakerandroidFall25Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            IcebreakerandroidFall25Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    IcebreakerandroidFall25Theme {
+        Greeting("Android")
+    }
+}
