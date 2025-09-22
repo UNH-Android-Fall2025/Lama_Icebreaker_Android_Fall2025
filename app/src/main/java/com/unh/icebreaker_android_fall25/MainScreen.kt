53476c498a16112acf58a3ff0c2b796a5c9ebb68
CopyCodeWithMe <razzzzzlama@gmail.com>
2025-09-21 21:58:56 -0400

    Added text entry, string resources
    I added the full title and the first text entry field, then I removed hard-coded string values and replaced them with calls to the string resource file.

--- app/src/main/java/com/unh/icebreaker_android_fall25/MainScreen.kt
+++ app/src/main/java/com/unh/icebreaker_android_fall25/MainScreen.kt
@@ -4,10 +4,17 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -21,8 +28,24 @@ fun MainScreen() {
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(
-            text = "IceBreaker!",
-            fontSize = 36.sp
+            text = stringResource(R.string.header_text),
+            fontSize = 32.sp,
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text = stringResource(R.string.header_sub_text),
+            fontSize = 28.sp,
+            fontWeight = FontWeight.Normal
+        )
+        var firstName by remember { mutableStateOf(""}
+
+        OutlinedTextField(
+            enabled = true,
+            singleLine = true,
+            modifier = Modifier.padding(top = 50.dp),
+            value = firstName,
+            onValueChange = {firstName = it},
+            label = {Text(stringResource(R.string.first_name_text))}
         )
     }
 }
